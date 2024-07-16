db44421a824fa5f4e729d64bf1d7c6d016f7a087
Mohammed Yousuf <mohammedyousuf16.md@gmail.com>
2024-07-16 12:54:55 +0530

    Data types

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+/*
+
 let js = "amazing";
-console.log(40 + 8 - 23 + 10);
+//console.log(40 + 8 - 23 + 10);
 
 console.log("yousuf");
 console.log(23);
@@ -10,6 +12,7 @@ console.log(firstName);
 console.log(firstName);
 console.log(firstName);
 
+// variable name covertions
 let mohammed_yousuf = "MY";
 let $function = 23;
 
@@ -20,3 +23,26 @@ let myFirstJob = "programmer";
 let myCurrentJob = "student";
 
 console.log(myFirstJob);
+*/
+
+let a = 23;
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof "java");
+
+javascriptIsFun = "Yes!";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof null);
