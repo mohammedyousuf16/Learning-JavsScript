44523cea5329523553b596eff1ef249d0562e351
Mohammed Yousuf <mohammedyousuf16.md@gmail.com>
2024-07-16 13:51:57 +0530

    basic operators

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -47,7 +47,6 @@ console.log(typeof year);
 
 console.log(typeof null);
 
-*/
 
 let age = 30;
 age = 31;
@@ -59,3 +58,35 @@ const birthYear = 1991;
 
 var job = "programmer";
 job = "student";
+
+*/
+//Math operators
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = "james";
+const lastName = "bond";
+
+console.log(firstName + " " + lastName);
+
+//Assignment operators
+let x = 10 + 5; //x=15 here
+x += 10; //x=15+10=25
+x *= 4; //x=25*4
+x++; //x=x+1
+x--;
+x--;
+console.log(x);
+
+//comparison operators
+console.log(ageJonas > ageSarah); // >, <, >=, <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
