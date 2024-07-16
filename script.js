c6c14282652566d4e03321db948dec4b5c9fa7c6
Mohammed Yousuf <mohammedyousuf16.md@gmail.com>
2024-07-16 12:28:39 +0530

    Values and variables

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 let js = "amazing";
-if (js === "amazing") alert("javascript is FUN");
-else alert("javascript is boring");
-
 console.log(40 + 8 - 23 + 10);
+
+console.log("yousuf");
+console.log(23);
+
+let firstName = "yousuf";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let mohammed_yousuf = "MY";
+let $function = 23;
+
+let person = "mohammed";
+let PI = 3.1415;
+
+let myFirstJob = "programmer";
+let myCurrentJob = "student";
+
+console.log(myFirstJob);
