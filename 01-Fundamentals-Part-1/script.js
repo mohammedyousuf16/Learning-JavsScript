d49ed012210320175027bcce758ffea8f2371e2d
Mohammed Yousuf <mohammedyousuf16.md@gmail.com>
2024-07-16 13:20:22 +0530

    foldered the files till now

---
+++ 01-Fundamentals-Part-1/script.js
@@ -0,0 +1,61 @@
+/*
+
+let js = "amazing";
+//console.log(40 + 8 - 23 + 10);
+
+console.log("yousuf");
+console.log(23);
+
+let firstName = "yousuf";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// variable name covertions
+let mohammed_yousuf = "MY";
+let $function = 23;
+
+let person = "mohammed";
+let PI = 3.1415;
+
+let myFirstJob = "programmer";
+let myCurrentJob = "student";
+
+console.log(myFirstJob);
+
+
+let a = 23;
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof "java");
+
+javascriptIsFun = "Yes!";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof null);
+
+*/
+
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990;
+
+// const job;
+
+var job = "programmer";
+job = "student";
