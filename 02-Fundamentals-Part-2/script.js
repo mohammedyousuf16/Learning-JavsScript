2dd74f7fc68f88c8b726d7aaafb4a51f53babae4
Mohammed Yousuf <mohammedyousuf16.md@gmail.com>
2024-07-17 16:51:32 +0530

    activating strick mode

---
+++ 02-Fundamentals-Part-2/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log(`i can drive`);
+
+// const interface = "audio";
+// const private = "audio";
