50f81cbb103866aa7a0b3b0963faa63d366697d7
Mohammed Yousuf <mohammedyousuf16.md@gmail.com>
2024-07-18 11:55:04 +0530

    function Declarations and Expressions

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -31,3 +31,17 @@ const applaOrangeJuice = fruitProcessor(2, 4);
 console.log(applaOrangeJuice);
 
 */
+
+// Function declaration
+function calcAge1(birthYear) {
+  return 2037 - birthYear;
+}
+
+const age1 = calcAge1(1991);
+
+// Function expression
+const calcAge2 = function (birthYear) {
+  return 2037 - birthYear;
+};
+const age2 = calcAge2(1991);
+console.log(age1, age2);
