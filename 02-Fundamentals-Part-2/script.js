43040a7c8d67db1f69378c5df1bd80e9255ecfe0
Mohammed Yousuf <mohammedyousuf16.md@gmail.com>
2024-07-18 11:43:22 +0530

    functions

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -1,5 +1,5 @@
 "use strict";
-
+/*
 let hasDriversLicense = false;
 const passTest = true;
 
@@ -8,3 +8,26 @@ if (hasDriversLicense) console.log(`i can drive`);
 
 // const interface = "audio";
 // const private = "audio";
+
+//functions
+
+function logger() {
+  console.log("my name is mohammed yousuf");
+}
+//calling /running / invoking funtion
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  const juice = `juice with ${apples} apples and ${oranges} oranges`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const applaOrangeJuice = fruitProcessor(2, 4);
+console.log(applaOrangeJuice);
+
+*/
