3f90272906a389cdfe47b7ec5f070a2d861600ba
Mohammed Yousuf <mohammedyousuf16.md@gmail.com>
2024-07-25 22:33:11 +0530

    Problem solving

--- 03-Developer-Skills/script.js
+++ 03-Developer-Skills/script.js
@@ -2,7 +2,24 @@
 'use strict';
 
 const x = 23;
-if (x === 23) console.log(23);
+// if (x === 23) console.log(23);
 
 const calcAge = birthyear => 2037 - birthyear;
-console.log();
+
+// problem
+
+const tepratures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+let total = 0;
+const tempAmplitude = function (arr) {
+  for (let i = 0; i < arr.length; i++) {
+    const temp = arr[i];
+    if (arr[i] === 'error') {
+      console.log(`"error" found in array at ${arr[i].length - 1}`);
+    } else {
+      total += temp;
+    }
+  }
+  console.log(`the total value is ${total} after adding all the array items`);
+};
+
+console.log(tempAmplitude(tepratures));
