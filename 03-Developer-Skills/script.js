f46991ed8534e53dabe3462ee57f52bac4f8c798
Mohammed Yousuf <mohammedyousuf16.md@gmail.com>
2024-07-25 22:34:07 +0530

    challange 1

--- 03-Developer-Skills/script.js
+++ 03-Developer-Skills/script.js
@@ -23,3 +23,16 @@ const tempAmplitude = function (arr) {
 };
 
 console.log(tempAmplitude(tepratures));
+// if (arr[i] === 'error') {
+//   console.log(`string found: ${arr[i]}`);
+// } else {
+//   console.log('number');
+// }
+// Challange 1
+const arr = [12, 5, -5, 0, 4];
+const printForecast = function (arr) {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(`${arr[i]}ºC in ${i + 1} days`);
+  }
+};
+console.log(printForecast(arr));
