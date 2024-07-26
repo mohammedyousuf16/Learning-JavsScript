644dfdd7ff64b9f8dae18768b7ec283518ee6e3a
Mohammed Yousuf <mohammedyousuf16.md@gmail.com>
2024-07-26 11:54:42 +0530

    problem 2 -finding amplitude with 2 arrays

--- 03-Developer-Skills/script.js
+++ 03-Developer-Skills/script.js
@@ -6,33 +6,56 @@ const x = 23;
 
 const calcAge = birthyear => 2037 - birthyear;
 
-// problem
+// Challange 1
+// const arr = [12, 5, -5, 0, 4];
+// const printForecast = function (arr) {
+//   for (let i = 0; i < arr.length; i++) {
+//     console.log(`${arr[i]}ºC in ${i + 1} days`);
+//   }
+// };
+// console.log(printForecast(arr));
+
+// problem 1
 
 const tepratures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
-let total = 0;
-const tempAmplitude = function (arr) {
-  for (let i = 0; i < arr.length; i++) {
-    const temp = arr[i];
-    if (arr[i] === 'error') {
-      console.log(`"error" found in array at ${arr[i].length - 1}`);
-    } else {
-      total += temp;
-    }
+
+const calcTempAmplitude = function (temps) {
+  let max = temps[0];
+  let min = temps[0];
+
+  for (let i = 0; i < tepratures.length; i++) {
+    const curTemp = temps[i];
+    if (typeof temps[i] !== 'number') continue;
+    if (max < curTemp) max = curTemp;
+    if (min > curTemp) min = curTemp;
   }
-  console.log(`the total value is ${total} after adding all the array items`);
+  console.log(max, min);
+  return max - min;
 };
 
-console.log(tempAmplitude(tepratures));
-// if (arr[i] === 'error') {
-//   console.log(`string found: ${arr[i]}`);
-// } else {
-//   console.log('number');
-// }
-// Challange 1
-const arr = [12, 5, -5, 0, 4];
-const printForecast = function (arr) {
-  for (let i = 0; i < arr.length; i++) {
-    console.log(`${arr[i]}ºC in ${i + 1} days`);
+// const amplitude = calcTempAmplitude(tepratures);
+// console.log(amplitude);
+
+//  problem 2
+//  function 2 should now receive 2 arrays of temprature
+
+const calcTempAmplitudeNew = function (t1, t2) {
+  const temps = t1.concat(t2);
+  console.log(temps);
+  let max = temps[0];
+  let min = temps[0];
+
+  for (let i = 0; i < tepratures.length; i++) {
+    const curTemp = temps[i];
+    if (typeof temps[i] !== 'number') continue;
+    if (max < curTemp) max = curTemp;
+    if (min > curTemp) min = curTemp;
   }
+  console.log(max, min);
+  return max - min;
 };
-console.log(printForecast(arr));
+const amplitudeNew = calcTempAmplitudeNew(
+  [1, 3, 2, -5, 7, 9],
+  [3, 65, 8, 9, 3, 4]
+);
+console.log(amplitudeNew);
