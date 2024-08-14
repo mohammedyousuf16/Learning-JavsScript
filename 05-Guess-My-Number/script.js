37ad91f0d2602511fecdf189f7737f64568fb6ff
Mohammed Yousuf <mohammedyousuf16.md@gmail.com>
2024-08-14 16:48:47 +0530

    Implementing game logic

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -11,11 +11,39 @@ document.querySelector('.guess').value = 12;
 console.log(document.querySelector('.guess').value);
 */
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore;
+document.querySelector('.number').textContent = secretNumber;
+document.querySelector('.score').textContent = score;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(typeof guess, guess);
 
   if (!guess) {
     document.querySelector('.message').textContent = 'pls enter a number';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'correct number';
+    // highscore = score;
+    // document.querySelector('.highscore').textContent = highscore;
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too high';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'you lost pls refresh';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too Low';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'you lost pls refresh';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
