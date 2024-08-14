7e94c57a4263fd0c4a97797418ea20401c0c7ecf
Mohammed Yousuf <mohammedyousuf16.md@gmail.com>
2024-08-14 16:13:02 +0530

    Handeling click events

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = 'correct number!';
 
@@ -8,3 +9,13 @@ document.querySelector('.score').textContent = 10;
 
 document.querySelector('.guess').value = 12;
 console.log(document.querySelector('.guess').value);
+*/
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(typeof guess, guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'pls enter a number';
+  }
+});
