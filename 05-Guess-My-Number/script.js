ff1a400780f64ee77c95445894a7c1be311e12cd
Mohammed Yousuf <mohammedyousuf16.md@gmail.com>
2024-08-15 10:49:46 +0530

    Coding challange 1

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -11,12 +11,12 @@ document.querySelector('.guess').value = 12;
 console.log(document.querySelector('.guess').value);
 */
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-let highscore;
+let highscore = 0;
 
 document.querySelector('.score').textContent = score;
-
+document.querySelector('.highscore').textContent = highscore;
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(typeof guess, guess);
@@ -29,6 +29,10 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
     document.querySelector('.number').textContent = secretNumber;
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
     //when guess is too low
   } else if (guess > secretNumber) {
     if (score > 1) {
@@ -50,4 +54,15 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = 0;
     }
   }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.message').textContent = 'start guessing';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
 });
