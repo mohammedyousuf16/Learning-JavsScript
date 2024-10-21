77fd5d7cc76ebcacea7615d412b28d61730ab454
Mohammed Yousuf <mohammedyousuf16.md@gmail.com>
2024-10-21 11:09:12 +0530

    scoping in practice

---
+++ 07-Behind-the-Scenes/script.js
@@ -0,0 +1,35 @@
+// 'use strict';
+
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+
+  function printAge() {
+    let output = `${firstName} you are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;
+      // creating new variable with same name as outer scopes variables
+      const firstName = 'yousuf';
+      //reassigning outer scope variable
+      output = `new output`;
+      const str = `oh, and you are a millenial, ${firstName}`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+    }
+    console.log(millenial);
+    // console.log(add(2, 3));
+    console.log(output);
+  }
+  printAge();
+
+  return age;
+}
+
+const firstName = 'mohammed';
+calcAge(1991);
+//console.log(age);
+//printAge();
