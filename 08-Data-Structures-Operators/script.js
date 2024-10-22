4fc715201a4a4da95de6f714402bdf9d1e90dbcd
Mohammed Yousuf <mohammedyousuf16.md@gmail.com>
2024-10-22 11:50:51 +0530

    Destructuring Objects

--- 08-Data-Structures-Operators/script.js
+++ 08-Data-Structures-Operators/script.js
@@ -14,8 +14,82 @@ const restaurant = {
   order: function (starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    address,
+    time = '20:00',
+  }) {
+    console.log(
+      `order received ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be deleverd to ${address} at ${time}`
+    );
+  },
 };
 
+restaurant.orderDelivery({
+  time: '22:30',
+  address: 'vitamin address',
+  mainIndex: 2,
+  starterIndex: 2,
+});
+
+restaurant.orderDelivery({
+  address: 'vitamin D3 address',
+
+  starterIndex: 1,
+});
+
+const { name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories);
+
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+console.log(restaurantName, hours, tags);
+//default values
+
+const { menu = {}, starterMenu: starters = [] } = restaurant;
+console.log(menu, starters);
+
+//mutating variables
+
+let a = 111;
+let b = 999;
+const obj = { a: 23, b: 7, c: 24 };
+({ a, b } = obj);
+console.log(a, b);
+
+// Nested objects
+const {
+  fri: { open: o, close: c },
+} = openingHours;
+console.log(o, c);
+
+///
+/////
+///////
+//////////
+/////////////
+/////////////////
+////////////////////////
+////////////////////////////
+/*
 const arr = [2, 3, 4];
 const a = arr[0];
 const b = arr[1];
@@ -54,3 +128,4 @@ console.log(i, j, k);
 // Default values
 const [p = 1, q = 1, r = 1] = [8, 9];
 console.log(p, q, r);
+*/
