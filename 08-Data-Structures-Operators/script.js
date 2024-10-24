deb483a8b9c54bc777ad08070d6983a4ef2d341b
Mohammed Yousuf <mohammedyousuf16.md@gmail.com>
2024-10-24 12:33:29 +0530

    Sets

--- 08-Data-Structures-Operators/script.js
+++ 08-Data-Structures-Operators/script.js
@@ -50,6 +50,36 @@ const restaurant = {
     console.log(mainIngredians, otherIngrediants);
   },
 };
+
+const ordersSet = new Set([
+  'pasta',
+  'pizza',
+  'pasta',
+  'Risotto',
+  'pasta',
+  'pizza',
+]);
+console.log(ordersSet);
+
+console.log(new Set('james'));
+console.log(ordersSet.size);
+console.log(ordersSet.has('pizza'));
+console.log(ordersSet.has('bread'));
+ordersSet.add('garlic bread');
+ordersSet.add('garlic bread');
+ordersSet.delete('Risotto');
+// ordersSet.clear();
+console.log(ordersSet);
+for (const order of ordersSet) console.log(order);
+
+//Example
+const staff = ['waiter', 'chef', 'waiter', 'manager', 'chef', 'waiter'];
+const staffUnique = [...new Set(staff)];
+console.log(staffUnique);
+console.log(
+  new Set(['waiter', 'chef', 'waiter', 'manager', 'chef', 'waiter']).size
+);
+console.log(new Set('mohammedyousuf').size);
 /*
 // propertiy names
 const properties = Object.keys(openingHours);
