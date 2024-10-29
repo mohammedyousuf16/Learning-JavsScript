b72a383620380e4aaeb0ffa2721c409932b13848
Mohammed Yousuf <mohammedyousuf16.md@gmail.com>
2024-10-29 19:54:56 +0530

    Working with strings part-3

--- 08-Data-Structures-Operators/script.js
+++ 08-Data-Structures-Operators/script.js
@@ -50,7 +50,51 @@ const restaurant = {
     console.log(mainIngredians, otherIngrediants);
   },
 };
+// split and join
+console.log('a+very+nice+string'.split('+'));
+console.log('mohammed yousuf'.split(' '));
+const [firstName, lastName] = 'mohammed yousuf'.split(' ');
 
+const newName = ['Mr.', firstName, lastName.toUpperCase()].join(' ');
+console.log(newName);
+
+const capitalizeName = function (name) {
+  const names = name.split(' ');
+  const namesUpper = [];
+  for (const n of names) {
+    // namesUpper.push(n[0].toUpperCase() + n.slice(1));
+    namesUpper.push(n.replace(n[0], n[0].toUpperCase()));
+  }
+  console.log(namesUpper.join(' '));
+};
+capitalizeName('mohammed yousuf');
+capitalizeName('jessica ann smith davis');
+
+// padding a string
+const message = 'Go to gate 23';
+console.log(message.padStart(25, ' + ').padEnd(35, ' + '));
+console.log('mohammed'.padStart(25, '-').padEnd(35, ' + '));
+
+// Masking
+const maskCreditCard = function (number) {
+  const str = number + '';
+  const last = str.slice(-4);
+  return last.padStart(str.length, '*');
+};
+console.log(maskCreditCard(1234445353553535));
+console.log(maskCreditCard('134245353535534423424'));
+
+// Reapeat
+const message2 = 'Bad weather.... All Depature delayed... ';
+console.log(message2.repeat(5));
+
+const planesInLine = function (n) {
+  console.log(`there are ${n} planes in line `.repeat(n));
+};
+planesInLine(5);
+planesInLine(3);
+planesInLine(12);
+/*
 const airline = 'Tap Air portugal';
 console.log(airline.toLowerCase());
 console.log(airline.toUpperCase());
@@ -109,7 +153,7 @@ const checkBaggage = function (item) {
 checkBaggage('i have a laptop, some Food and a pocket Knife');
 checkBaggage('i have some clothes, shoes and laptop');
 checkBaggage('Got some snacks and a gun for protection');
-/*
+
 // Working with strings
 const airline = 'Tap Air portugal';
 const plane = 'A320';
