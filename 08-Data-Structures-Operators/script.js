c4597d613525cf95f508e08157edc24d517ea108
Mohammed Yousuf <mohammedyousuf16.md@gmail.com>
2024-11-05 12:05:02 +0530

    challenge #4

--- 08-Data-Structures-Operators/script.js
+++ 08-Data-Structures-Operators/script.js
@@ -50,6 +50,7 @@ const restaurant = {
     console.log(mainIngredians, otherIngrediants);
   },
 };
+/*
 // split and join
 console.log('a+very+nice+string'.split('+'));
 console.log('mohammed yousuf'.split(' '));
@@ -94,7 +95,8 @@ const planesInLine = function (n) {
 planesInLine(5);
 planesInLine(3);
 planesInLine(12);
-/*
+
+
 const airline = 'Tap Air portugal';
 console.log(airline.toLowerCase());
 console.log(airline.toUpperCase());
@@ -780,4 +782,49 @@ for (const [key, value] of gameEvents) {
     console.log(`Second Half: ${key}: ${value}`);
   }
 }
+
+Coding Challenge #4
+
+Write a program that receives a list of variable names written in underscore_case
+and convert them to camelCase.
+The input will come from a textarea inserted into the DOM (see code below to
+insert the elements), and conversion will happen when the button is pressed.
+Test data (pasted to textarea, including spaces):
+underscore_case
+first_name
+Some_Variable
+ calculate_AGE
+delayed_departure
+Should produce this output (5 separate console.log outputs):
+underscoreCase......✅
+firstName...........✅✅
+someVariable........✅✅✅
+calculateAge........✅✅✅✅
+delayedDeparture....✅✅✅✅✅
+Hints:
+§ Remember which character defines a new line in the textarea �
+§ The solution only needs to work for a variable made out of 2 words, like a_b
+§ Start without worrying about the ✅. Tackle that only after you have the variable
+name conversion working �
+§ This challenge is difficult on purpose, so start watching the solution in case
+you're stuck. Then pause and continue!
+Afterwards, test with your own test data!
+GOOD LUCK �
 */
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+
+document.querySelector('button').addEventListener('click', function () {
+  const text = document.querySelector('textarea').value;
+  const rows = text.split('\n');
+
+  for (const [i, row] of rows.entries()) {
+    const [first, second] = row.toLocaleLowerCase().trim().split('_');
+    // console.log(row, first, second);
+    const output = `${first}${second.replace(
+      second[0],
+      second[0].toUpperCase()
+    )}`;
+    console.log(`${output.padEnd(20)}${'@'.repeat(i + 1)}`);
+  }
+});
