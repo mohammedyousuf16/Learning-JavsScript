b7a9b9396a4e1d29f95d26931f15c2ac98c581e3
mohammedyousuf16 <mohammedyousuf16.md@gmail.com>
2026-09-09 20:18:31 +0530

    Logic for Draw has been added

--- Tic Tac Toe/app.js
+++ Tic Tac Toe/app.js
@@ -1,7 +1,11 @@
 let boxes = document.querySelectorAll(".box");
 let resetBtn = document.querySelector("#reset-btn");
+let newGameBtn = document.querySelector("#new-btn");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
 
-let playerO = true;
+let count = 0;
+let playerO = false;
 let winPatten = [
   [0, 1, 2],
   [3, 4, 5],
@@ -13,6 +17,13 @@ let winPatten = [
   [2, 4, 6],
 ];
 
+function resetGame() {
+  playerO = false;
+  enableBoxes();
+  count = 0;
+  msgContainer.classList.add("hide");
+}
+
 boxes.forEach((box) => {
   box.addEventListener("click", () => {
     if (playerO) {
@@ -23,20 +34,55 @@ boxes.forEach((box) => {
       playerO = true;
     }
     box.disabled = true;
+    count++;
     checkwinner();
+
+    let isWinner = checkwinner();
+    if (count === 9 && !isWinner) {
+      showDraw();
+    }
   });
 });
 
+function disableBoxes() {
+  boxes.forEach((box) => {
+    box.disabled = true;
+  });
+}
+
+function enableBoxes() {
+  boxes.forEach((box) => {
+    box.disabled = false;
+    box.innerText = "";
+  });
+}
+
+function showDraw() {
+  msg.innerText = `Game was a Draw`;
+  msgContainer.classList.remove("hide");
+}
+
+const showWinner = (winner) => {
+  msg.innerText = `Congragulation winner is ${winner}`;
+  msgContainer.classList.remove("hide");
+};
+
 const checkwinner = () => {
-  for (patten of winPatten) {
+  let hasWinner = false;
+  for (let patten of winPatten) {
     let pos1Val = boxes[patten[0]].innerText;
     let pos2Val = boxes[patten[1]].innerText;
     let pos3Val = boxes[patten[2]].innerText;
-    console.log(pos1Val, pos2Val, pos3Val);
     if (pos1Val !== "" && pos2Val !== "" && pos3Val !== "") {
       if (pos1Val === pos2Val && pos2Val === pos3Val) {
-        console.log("winner", pos1Val);
+        disableBoxes();
+        showWinner(pos1Val);
+        hasWinner = true;
       }
     }
   }
+  return hasWinner;
 };
+
+newGameBtn.addEventListener("click", resetGame);
+resetBtn.addEventListener("click", resetGame);
